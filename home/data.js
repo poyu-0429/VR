94470758e776f96e323091a275566c48435b5b21
poyu-0429 <nick152729@gmail.com>
2025-11-28 11:28:42 +0800

    1128

---
+++ home/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "大門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 1.2635538805072493,
+        "pitch": 0.027981111258398528,
+        "fov": 1.2599180821480807
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 1.0958672111251957,
+          "pitch": -0.3219261157137723,
+          "title": "藤子不二雄博物館",
+          "text": "<p class=\"MsoNormal\"><span style=\"font-family:&quot;jf open 粉圓 1.1&quot;,sans-serif\">藤子·<span lang=\"EN-US\">F</span>·不二雄博物館是為紀念日本漫畫家藤子·<span lang=\"EN-US\">F</span>·不二雄而成立的博物館，內部保存藤子・<span lang=\"EN-US\">F</span>・不二雄從<span lang=\"EN-US\">1961</span>年開始到過世的<span lang=\"EN-US\">1996</span>年共<span lang=\"EN-US\">35</span>年間的各項創作資料。位於日本神奈川縣川崎市多摩區於<span lang=\"EN-US\">2011</span>年<span lang=\"EN-US\">9</span>月<span lang=\"EN-US\">3</span>日開始對外開放。<span lang=\"EN-US\"><o:p></o:p></span></span></p>"
+        }
+      ]
+    }
+  ],
+  "name": "入口",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
